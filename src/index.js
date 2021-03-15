f528ba28a1bc2f4e2ac0388ee6aa3b73c0b1cf6b
Andrei Drobysh <andrey.drobysh.04@gmail.com>
2021-03-15 19:53:23 +0300

    finish reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let x = n;
+  if (n < 0) {
+    x = n * -1
+  }
+  let newStr = "";
+  let str = String(x);
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+
+  return newStr;
 }
